98477dc4f55878374d390541c2589f4f1600eb4a
reyes502rodrigo@gmail.com <reyes502rodrigo@gmail.com>
2024-10-08 22:41:24 -0600

    Instalacion de paquetes mysql, express y sequelize, se agrego txt con query para la creacion de la base de datos de resgistro, esto sujeto a cambios

--- src/components/PatientRegistration.js
+++ src/components/PatientRegistration.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PatientRegistration() {
   return (
     <div className="module-container">
-      <h2>Registro de Citas</h2>
+      <h2>Registro de Pacientes</h2>
       <p>Gestiona la información básica de las citas médicas</p>
       <button className="btn btn-primary">Añadir Cita</button>
     </div>

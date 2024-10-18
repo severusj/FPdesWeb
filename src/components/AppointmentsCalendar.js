71316a6627c8c45cb2adf5cf84ef65166b831de4
James Gonzalez <james19942009@hotmail.com>
2024-10-18 13:04:21 -0600

    MDF: Add the call to API on frontend

--- src/components/AppointmentsCalendar.js
+++ src/components/AppointmentsCalendar.js
@@ -1,7 +1,7 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import axios from 'axios'; // Importa axios para hacer la petición GET
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './AppointmentsCalendar.css';
 
@@ -9,6 +9,30 @@ const localizer = momentLocalizer(moment);
 
 function AppointmentsCalendar() {
   const [events, setEvents] = useState([]);
+
+  // useEffect para obtener datos del servidor
+  useEffect(() => {
+    const fetchPatients = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/get-patient');
+        const patients = response.data;
+        console.log(patients)
+        // Mapeamos los datos obtenidos en eventos para el calendario
+        const appointments = patients.map((patient) => ({
+          title: `${patient.Nombre_1} ${patient.Apellido_1}`, 
+          start: new Date(patient.Fecha_Cita + ' ' + patient.Hora_Cita), 
+          end: new Date(patient.Fecha_Cita + ' ' + patient.Hora_Cita),
+        }));
+
+        // Actualiza el estado con los eventos
+        setEvents(appointments);
+      } catch (error) {
+        console.error('Error al obtener los pacientes:', error);
+      }
+    };
+
+    fetchPatients();
+  }, []);
 
   return (
     <div className="module-container">
